f248dce99b6a3c37ee7ed46eac296daebf794607
Sergei ★ Česnakas <10352420+cesnakas@users.noreply.github.com>
2021-09-19 01:59:06 +0400

    Update slider section

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,18 @@
 const swiper = new Swiper ('.swiper', {
-    slidesPerView: 4,
-    spaceBetween: 20,
+    breakpoints: {
+        576: {
+            slidesPerView: 2,
+            spaceBetween: 20
+        },
+        768: {
+            slidesPerView: 3,
+            spaceBetween: 20
+        },
+        992: {
+            slidesPerView: 4,
+            spaceBetween: 20
+        }
+    },
     // loop: true,
     navigation: {
         nextEl: ".swiper-button-next",
